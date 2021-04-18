4abdd17e23735ec49e970a53e4e473e2a21eac25
laya-star <78084935+laya-star@users.noreply.github.com>
2021-04-18 16:48:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+rightWristX="";
+rightWristY="";
+rightWristScore="";
 /*created by prashant shukla */
 
 var paddle2 =10,paddle1=10;
@@ -26,8 +28,17 @@ function setup(){
   canvas.parent('canvas');
   video=createCapture(VIDEO);
   video.size(700,600);
+  video.parent('canvas');
   video.hide();
   poseNet=ml5.poseNet(video,modelLoaded);
+  poseNet.on('poses',gotPoses);
+}
+function gotPoses(){
+  if(results.length>0){
+    rightWristX=results[0].pose.rightWrist.x;
+    rightWristY=results[0].pose.rightWrist.y;
+    rightWristScore=results[0].pose.keypoints[10].score;
+  }
 }
 function modelLoaded(){
   console.log("Model Loaded!");
@@ -36,8 +47,12 @@ function modelLoaded(){
 
 function draw(){
 image(video,0,0,700,600);
-
- background(0); 
+if(rightWristScore>0.2){
+  fill("red");
+  stroke("dark red");
+  circle(rightWristX, rightWristY, 30);
+}
+  
 
  fill("black");
  stroke("black");
